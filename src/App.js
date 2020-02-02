bf1b7754cdd25d5851dedfb2dc101027d322c371
Alex Bieg <alexbieg95@gmail.com>
2020-02-02 13:58:55 -0800

    added loading animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import THINGS from './things.js';
 import { getWeekNumber, useInterval, getTimeText, getDateText } from './utils';
@@ -6,10 +6,23 @@ import { getWeekNumber, useInterval, getTimeText, getDateText } from './utils';
 const weekNum = getWeekNumber(new Date());
 const baseBackgroundURL = 'background/sputnik-';
 const backgroundChoice = Math.floor(Math.random() * 10 + 1);
+const url = `${baseBackgroundURL}${backgroundChoice}.jpg`;
+
 
 function App() {
   const [date, setDate] = useState(new Date());
   const [currWeek, setCurrWeek] = useState(weekNum);
+  const [backgroundUrl, setBackgroundUrl] = useState('');
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setBackgroundUrl(url)
+    }
+    img.src = url;
+    if (img.complete) img.onload();
+    console.log('adf');
+  }, []);
 
   useInterval(() => {
     setDate(new Date());
@@ -24,7 +37,8 @@ function App() {
   }
 
   return (
-    <div className="App" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${baseBackgroundURL}${backgroundChoice}.jpg)` }}>
+    <div className="App">
+      <div className={`background${backgroundUrl.length ? '' : ' hidden'}`} style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${backgroundUrl})` }}/>
       <div className="date">
         <div>{ getDateText(date) }</div>
         <div>{ getTimeText(date) }</div>
