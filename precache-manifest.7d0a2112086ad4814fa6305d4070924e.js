c4a77e812396da5837aaf4681c3da9b4ca716aa0
Alex Bieg <alexbieg95@gmail.com>
2020-02-01 17:28:11 -0800

    Updates

---
+++ precache-manifest.7d0a2112086ad4814fa6305d4070924e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c149b4ebc8acef046c2369753b5d18e",
+    "url": "/kenzie-start-page/index.html"
+  },
+  {
+    "revision": "f2a071427d168488f2d0",
+    "url": "/kenzie-start-page/static/css/main.bf95fc68.chunk.css"
+  },
+  {
+    "revision": "dec13f0646a9a904330f",
+    "url": "/kenzie-start-page/static/js/2.02e10f7a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kenzie-start-page/static/js/2.02e10f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a071427d168488f2d0",
+    "url": "/kenzie-start-page/static/js/main.2248a020.chunk.js"
+  },
+  {
+    "revision": "6497efbcc1924b316da8",
+    "url": "/kenzie-start-page/static/js/runtime-main.f8b713bf.js"
+  }
+]);
